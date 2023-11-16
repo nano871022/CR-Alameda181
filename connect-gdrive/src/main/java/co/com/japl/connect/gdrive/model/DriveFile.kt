8612540ffce67b218b19b4cbcc083e3f545c74a4
alejandro parra <alejandroparral@gmail.com>
2023-11-16 01:26:00 -0500

    feature alias link modules
    remove code from project

--- connect-gdrive/src/main/java/co/com/japl/connect/gdrive/model/DriveFile.kt
+++
@@ -1,7 +0,0 @@
-package co.com.japl.connect.gdrive.model
-
-data class DriveFile(   val id: String,
-                        val name: String,
-                        val mimeType: String,
-                        val url: String
-                        )
